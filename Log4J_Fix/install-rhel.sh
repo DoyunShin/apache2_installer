e9ce90621b8ce3b2d18c34607d3cf5e919296c83
TDB <op@twitchdarkbot.com>
2021-12-14 09:56:48 +0900

    Updated to 2.4.51

---
+++ Log4J_Fix/install-rhel.sh
@@ -0,0 +1,11 @@
+mkdir work-apache2-inst
+cd work-apache2-inst
+apt install make gcc g++ libpcre3-dev libexpat1-dev build-essential zlib1g zlib1g-dev openssl m4 python3-dev gnulib curl -y
+echo Downloading httpd-2.4.51
+curl -LO https://dlcdn.apache.org/httpd/httpd-2.4.51.tar.gz
+tar xvzf httpd-2.4.51.tar.gz
+
+cd httpd-2.4.51/
+./configure --prefix=/usr/local/apache2 --enable-module=so --enable-mods-shared=all --enable-so --enable-deflate --enable-rewrite --enable-ssl --with-ssl=/usr/local/openssl-1.1.1g --with-apr=/usr/local/apache2 --with-apr-util=/usr/local/apache2
+make && make install
+cd ../../
--- install-deb.sh
+++ install-deb.sh
@@ -2,9 +2,12 @@ mkdir work-apache2-inst
 cd work-apache2-inst
 rm -rf ./*
 apt update
-apt install make gcc g++ libpcre3-dev libexpat1-dev build-essential zlib1g zlib1g-dev openssl m4 python3-dev gnulib -y
-curl -LO http://archive.apache.org/dist/httpd/httpd-2.4.43.tar.gz
-tar xvzf httpd-2.4.43.tar.gz
+apt install make gcc g++ libpcre3-dev libexpat1-dev build-essential zlib1g zlib1g-dev openssl m4 python3-dev gnulib curl -y
+
+echo Downloading httpd-2.4.51
+curl -LO https://dlcdn.apache.org/httpd/httpd-2.4.51.tar.gz
+tar xvzf httpd-2.4.51.tar.gz
+
 mkdir plugin
 cd plugin
 curl -LO https://ftp.pcre.org/pub/pcre/pcre2-10.35.tar.gz &
@@ -12,9 +15,10 @@ curl -LO http://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz &
 curl -LO http://ftp.gnu.org/gnu/automake/automake-1.15.tar.gz &
 curl -LO http://ftp.gnu.org/gnu/m4/m4-1.4.18.tar.gz &
 curl -LO https://www.openssl.org/source/openssl-1.1.1g.tar.gz &
-curl -LO http://apache.tt.co.kr//apr/apr-util-1.6.1.tar.gz &
 curl -LO http://mirror.jre655.com/GNU/libtool/libtool-2.4.6.tar.gz &
-curl -LO http://apache.tt.co.kr//apr/apr-1.7.0.tar.gz
+curl -LO https://dlcdn.apache.org//apr/apr-util-1.6.1.tar.gz &
+curl -LO https://dlcdn.apache.org//apr/apr-1.7.0.tar.gz
+
 fg
 fg
 fg
@@ -22,6 +26,7 @@ fg
 fg
 fg
 fg
+
 tar xvzf openssl-1.1.1g.tar.gz &
 tar xvzf libtool-2.4.6.tar.gz &
 tar xvzf m4-1.4.18.tar.gz &
@@ -30,6 +35,7 @@ tar xvzf autoconf-2.69.tar.gz &
 tar xvzf apr-util-1.6.1.tar.gz &
 tar xvzf automake-1.15.tar.gz &
 tar xvzf apr-1.7.0.tar.gz 
+
 fg
 fg
 fg
@@ -39,11 +45,11 @@ fg
 fg
 
 cd ./apr-1.7.0
-./configure --prefix=/usr/local/apache/apr
+./configure --prefix=/usr/local/apache2
 make && make install
 cd ../apr-util-1.6.1
 
-./configure --with-apr=/usr/local/apache/apr --prefix=/usr/local/apache/apr-util
+./configure --with-apr=/usr/local/apache2 --prefix=/usr/local/apache2
 make && make install
 cd ../openssl-1.1.1g
 
@@ -69,18 +75,18 @@ cd ../automake-1.15
 
 ./configure
 make && make install
-cd ../../httpd-2.4.43/
+cd ../../httpd-2.4.51/
 
-./configure --prefix=/usr/local/apache24 --enable-module=so --enable-mods-shared=all --enable-so --enable-deflate --enable-rewrite --enable-ssl --with-ssl=/usr/local/openssl-1.1.1g --with-apr=/usr/local/apache/apr --with-apr-util=/usr/local/apache/apr-util
+./configure --prefix=/usr/local/apache2 --enable-module=so --enable-mods-shared=all --enable-so --enable-deflate --enable-rewrite --enable-ssl --with-ssl=/usr/local/openssl-1.1.1g --with-apr=/usr/local/apache2 --with-apr-util=/usr/local/apache2
 make && make install
 cd ../
 
 ln -s /usr/local/apache24/bin/httpd /usr/local/bin/httpd
 
 cd plugin
-curl -LO https://github.com/GrahamDumpleton/mod_wsgi/archive/4.7.1.tar.gz
-tar xvzf 4.7.1.tar.gz
-cd mod_wsgi-4.7.1
-./configure --with-apxs=/usr/local/apache24/bin/apxs --with-python=$(which python3)
+curl -LO https://github.com/GrahamDumpleton/mod_wsgi/archive/refs/tags/4.9.0.tar.gz
+tar xvzf 4.9.0.tar.gz
+cd mod_wsgi-4.9.0
+./configure --with-apxs=/usr/local/apache2/bin/apxs --with-python=$(which python3)
 make && make install
 cd ../../
