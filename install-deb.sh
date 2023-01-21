e6bc54450928abdd962da7e99b77d46769700c0e
TDB <github@ontdb.com>
2023-01-21 18:24:54 +0900

    Update 2.4.54 to 2.4.55

--- install-deb.sh
+++ install-deb.sh
@@ -4,9 +4,9 @@ rm -rf ./*
 apt update
 apt install make gcc g++ libpcre3-dev libexpat1-dev build-essential zlib1g zlib1g-dev openssl m4 python3-dev gnulib curl -y
 
-echo Downloading httpd-2.4.54
-curl -LO https://dlcdn.apache.org/httpd/httpd-2.4.54.tar.gz
-tar xvzf httpd-2.4.54.tar.gz
+echo Downloading httpd-2.4.55
+curl -LO https://dlcdn.apache.org/httpd/httpd-2.4.55.tar.gz
+tar xvzf httpd-2.4.55.tar.gz
 
 mkdir plugin
 cd plugin
@@ -35,7 +35,7 @@ cd ../openssl-1.1.1g
 
 ./config --openssldir=/usr/local/openssl-1.1.1g
 make && make install
-cd ../../httpd-2.4.54/
+cd ../../httpd-2.4.55/
 
 ./configure --prefix=/usr/local/apache2 --enable-module=so --enable-mods-shared=all --enable-so --enable-deflate --enable-rewrite --enable-ssl --with-ssl=/usr/local/openssl-1.1.1g --with-apr=/usr/local/apache2 --with-apr-util=/usr/local/apache2
 make && make install
