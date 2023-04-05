bdd641e687f975200546d90708c6e87806f48203
TDB <github@ontdb.com>
2023-04-05 22:16:13 +0900

    php8 update

--- install-php8.sh
+++ install-php8.sh
@@ -5,6 +5,7 @@ cd work-apache2-inst
 mkdir plugin
 cd plugin
 apt install autoconf re2c bison libsqlite3-dev libpq-dev libonig-dev libfcgi-dev libfcgi0ldbl libjpeg-dev libpng-dev libssl-dev libxml2-dev libcurl4-openssl-dev libxpm-dev libgd-dev libmysqlclient-dev libfreetype6-dev libxslt1-dev libpspell-dev libzip-dev libgccjit-10-dev -y
+yum install git make gcc gcc-c++ binutils glibc-devel autoconf libtool bison re2c automake libxml2-devel openssl-devel libcurl-devel libjpeg-devel libpng-devel libXpm-devel freetype-devel libicu-devel libxslt-devel libzip-devel libgccjit-devel -y
 curl -L https://www.php.net/distributions/php-8.2.4.tar.gz | tar xz
 ./configure --with-apxs2=/usr/local/apache2/bin/apxs --enable-cli --enable-fpm --enable-intl --enable-mbstring --enable-opcache --enable-sockets --enable-soap --with-curl --with-freetype --with-fpm-user=www-data --with-fpm-group=www-data --with-jpeg --with-mysql-sock --with-mysqli --with-openssl --with-pdo-mysql --with-pgsql --with-xsl --with-zlib
 make -j $THREADS && make install -j $THREADS
